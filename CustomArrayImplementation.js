3a059d19449ef0617df2b52d88236c8d45a9413c
ayoubfatah <ayoubfatah222@gmail.com>
2024-12-31 20:27:07 +0100

    CustomArrayImplementation

---
+++ CustomArrayImplementation.js
@@ -0,0 +1,60 @@
+class Array {
+  constructor() {
+    this.length = 0;
+    this.data = {};
+  }
+  push(item) {
+    this.data[this.length] = item;
+    this.length++;
+
+    return this.length;
+  }
+  get(number) {
+    delete 2;
+    return this.data[number];
+  }
+  pop() {
+    const latestItem = this.data[this.length - 1];
+    delete this.data[this.length - 1];
+    this.length--;
+    return latestItem;
+  }
+  shift() {
+    const firstItem = this.data[0];
+
+    for (let i = 0; i < this.length; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+
+    delete this.data[this.length - 1];
+    this.length--;
+    return firstItem;
+  }
+  deleteByIndex(index) {
+    if (index < 0 || index >= this.length) {
+      throw new Error("Index out of bounds");
+    }
+
+    const deletedItem = this.data[index];
+
+    for (let i = index; i < this.length - 1; i++) {
+      this.data[i] = this.data[i + 1];
+    }
+
+    delete this.data[this.length - 1];
+    this.length--;
+
+    return deletedItem;
+  }
+}
+
+const myArray = new Array();
+myArray.push("orange");
+myArray.push("apple");
+myArray.push("banana");
+myArray.push("strawberry");
+myArray.push("mango");
+myArray.push("kiwi");
+myArray.deleteByIndex(5);
+
+console.log(myArray);
